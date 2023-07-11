ba768d2f39d5de213c502b2243638ea4903b9824
Darshan <darshanrao526@gmail.com>
2023-07-11 12:38:41 +0530

    social site
    social site in php

---
+++ themes/D-Theme/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'D-Theme';
+
+$themeFolder = 'D-Theme';
+
+$themeAuthor = 'Darshan Yadav';
+
+$themeAuthorUrl = 'mailto: darshanrao526@gmail.com';
+
+$themeVirsion = '1.0';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
